9035d0eeb132e9d61f484d590ce5933274a5e8d8
Bohdan Zhuravel <zhuravel.b@gmail.com>
2013-06-28 01:19:06 +0300

    Found time and inspiration

--- .oh-my-zsh/custom/plugins/zhuravel/zhuravel.plugin.zsh
+++ .oh-my-zsh/custom/plugins/zhuravel/zhuravel.plugin.zsh
@@ -6,44 +6,17 @@ h() { cd ~/$1; }
 _h() { _files -W ~/ -/; }
 compdef _h h
 
-# autocorrect is more annoying than helpful
+# Autocorrect is more annoying than helpful
 unsetopt correct_all
 
-# command line history
+# Command line history
 bindkey '^R' history-incremental-search-backward
 
-# add plugin's bin directory to path
+# Add plugin's bin directory to path
 export PATH="$(dirname $0)/bin:$PATH"
 
-# a few aliases I like
-alias ptt='rake parallel:test'
-alias pts='rake parallel:spec'
-alias combo='rake db:migrate parallel:migrate db:schema:dump'
-alias mysql='mysql -u root'
-alias mysql-dir='cd /usr/local/var/mysql/'
-alias psql='psql -U bohdan postgres'
-alias redis-status='redis-cli ping'
-for dbname in mysql postgresql couchdb redis; do
-  eval "${dbname}-start() { launchctl load ~/Library/LaunchAgents/homebrew.mxcl.${dbname}.plist }"
-  eval "${dbname}-stop() { launchctl unload ~/Library/LaunchAgents/homebrew.mxcl.${dbname}.plist }"
+# Load the shell dotfiles
+for file in "$(dirname $0)"/.{exports,aliases,functions}; do
+  [ -r "$file" ] && source "$file"
 done
-
-# who is listening on a given TCP port?
-# $ onport 5984
-# $ onport 5984 --pid
-onport() {
-  if [ $# -eq 2 ]; then
-    lsof -i TCP:$1 -t
-  else
-    lsof -i TCP:$1 | grep LISTEN
-  fi
-}
-
-# helps me figure out what I can delete to free some space on SSD
-find_large_files() {
-  if [ ! -z $1 ]; then
-    sudo find $1 -type f -size +${2:-100M} -exec du -h {} \; 2> /dev/null
-  else
-    echo "Usage: find_large_files /path 100M"
-  fi
-}
+unset file
--- .zshrc
+++ .zshrc
@@ -5,6 +5,5 @@ DISABLE_AUTO_UPDATE="true"
 plugins=(git osx colored-man gem brew go bundler rake rails3 zhuravel)
 
 export PATH="$PATH:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:/usr/local/sbin"
-export EDITOR="subl -w"
 
 source $ZSH/oh-my-zsh.sh
