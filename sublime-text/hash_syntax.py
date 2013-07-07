772b761953343178dfcd65ace5839d7c4e21d2a7
Bohdan Zhuravel <zhuravel.b@gmail.com>
2013-07-07 21:15:05 +0300

    Remove packages, update keymap, hash_syntax.py

---
+++ sublime-text/hash_syntax.py
@@ -0,0 +1,21 @@
+import sublime, sublime_plugin, re
+
+def new_style_hash(matchobj):
+  spaces = len(matchobj.group(3) + matchobj.group(4)) - 1
+  if spaces < 1: spaces = 1
+  return "%s%s:%s" % (matchobj.group(1), matchobj.group(2), spaces * ' ')
+
+class HashSyntax(sublime_plugin.TextCommand):
+  def run(self, edit):
+    if sum(len(region) for region in self.view.sel()) == 0:
+      region = sublime.Region(0, self.view.size())
+      self.replace_hashes(edit, region)
+
+    for region in self.view.sel():
+      if not region.empty():
+        self.replace_hashes(edit, region)
+
+  def replace_hashes(self, edit, region):
+    s = self.view.substr(region)
+    s = re.sub(r'([^\:])\:([a-zA-Z_0-9]*)(\s*)=\>(\s*)', new_style_hash, s)
+    self.view.replace(edit, region, s)
